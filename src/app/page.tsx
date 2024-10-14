c699a3a0cacff7b73b34d07b9315fe625faab6ea
arpitsahuu <arpitsahu358@gmail.com>
2024-10-14 13:01:38 +0530

    remoce extra spline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import CustomerOnboarding from '@/components/routers/Onbord';
 import { Industrys } from '@/components/about/Industrys';
 import ServicesSemi from '@/components/routers/ServiceSemi';
 import Impact from '@/components/routers/Impact';
-import Spline from '@splinetool/react-spline/next';
 
 export default function Home() {
 
@@ -19,10 +18,6 @@ export default function Home() {
 
 
       <Industrys />
-
-      <Spline className='w-[750px] h-[700px] lg:scale-[0.80] xl:scale-[0.95] md:scale-[0.70] sm:scale-[0.35] scale-[0.30]  '
-        scene="https://prod.spline.design/AytvQmZ1V7RFdW2j/scene.splinecode"
-      />
 
       {/* <Statistics /> */}
 
--- src/components/routers/Hero.tsx
+++ src/components/routers/Hero.tsx
@@ -29,7 +29,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="lg:w-[55%] w-full lg:h-full md:h-[500px] h-[300px]   flex justify-center items-center ">
+      <div className="lg:w-[55%] w-full lg:h-full md:h-[500px] h-[320px]   flex justify-center items-center ">
         <div className="relative w-[500px] h-[550px] sm: flex items-center justify-center overflow-hidden  md:overflow-visible " >
         <Spline className='w-[750px] h-[700px] lg:scale-[0.80] xl:scale-[1.40] md:scale-[0.70] sm:scale-[0.35] scale-[0.60]  '
               scene="https://prod.spline.design/AytvQmZ1V7RFdW2j/scene.splinecode"
